b36ddacfb4a56e571630c9a11da53e7c41a1664a
Juliette <71668581+J-IFT@users.noreply.github.com>
2022-10-06 14:03:19 +0200

    Add files via upload

---
+++ Projects/Serveur web engiv/src/index.h
@@ -0,0 +1,295 @@
+const char MAIN_page[] PROGMEM = R"=====(
+	
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>SENGIV</title>
+    </head>
+
+    <body>
+        <img class="logo" src="https://i.ibb.co/n8wsmw7/5-DA7-B2-FA-877-C-4420-B0-A9-F34140-AD4015.png" alt="5-DA7-B2-FA-877-C-4420-B0-A9-F34140-AD4015" border="0">
+
+      <div class= "container">
+        <div class="Tuyaux">Tuyau 1 : Vitis labrusca
+        <br><br>
+          <br> Température : <span id="Capteur">NA</span>°C <br>
+        <br><br>
+            <label class="switch">
+              <input type="checkbox">
+              <span class="slider round"></span>
+            </label>
+        </div>
+        <br><br>
+        <div class="Tuyaux">Tuyau 2 : Vitis rupestris
+        <br><br>
+          <br> Température : <span id="Capteur1">NA</span>°C <br>
+        <br><br>
+          <label class="switch">
+              <input type="checkbox">
+              <span class="slider round"></span>
+          </label>
+        </div>
+        <br><br>
+        <div class="Tuyaux">Tuyau 3 : Vitis berlandieri
+        <br><br>
+          <br> Température : <span id="Capteur2">NA</span>°C <br>
+        <br><br>
+          <label class="switch">
+              <input type="checkbox">
+              <span class="slider round"></span>
+          </label>
+        </div>
+        <br><br>
+        <div class="Tuyaux">Tuyau 4 : Vitis aestivalis
+        <br><br>
+          <br> Température : <span id="Capteur3">NA</span>°C <br>
+        <br><br>
+          <label class="switch">
+              <input type="checkbox">
+              <span class="slider round"></span>
+          </label>
+        </div>
+      </div>
+
+    </body>
+</html>
+
+<script>
+function sendData(led) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("Capteur").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "setLED?Capteur="+led, true);
+  xhttp.send();
+}
+
+setInterval(function() {
+  // Call a function repetatively with 2 Second interval
+  getData();
+}, 2000); //2000mSeconds update rate
+
+function getData() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("Capteur").innerHTML =
+      this.responseText;
+      document.getElementById("Capteur1").innerHTML =
+      this.responseText;
+      document.getElementById("Capteur2").innerHTML =
+      this.responseText;
+      document.getElementById("Capteur3").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "readCapteur", true);
+  xhttp.send();
+}
+
+</script>
+
+
+<style>
+
+/* #region ----- RESET CSS -----*/
+/* Box sizing rules */
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+/* Remove default margin */
+body,
+h1,
+h2,
+h3,
+h4,
+p,
+figure,
+blockquote,
+dl,
+dd {
+  margin: 0;
+}
+
+
+/* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */
+ul[role='list'],
+ol[role='list'] {
+  list-style: none;
+}
+
+/* Set core root defaults */
+html:focus-within {
+  scroll-behavior: smooth;
+}
+
+/* Set core body defaults */
+body {
+  min-height: 100vh;
+  text-rendering: optimizeSpeed;
+  line-height: 1.5;
+}
+
+/* A elements that don't have a class get default styles */
+a:not([class]) {
+  text-decoration-skip-ink: auto;
+}
+
+/* Make images easier to work with */
+img,
+picture {
+  max-width: 200%;
+  display: block;
+}
+
+/* Inherit fonts for inputs and buttons */
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
+/* #region ----- VARIABLES ----- */
+:root{
+  --clr-background: hsl(242, 28%, 20%);
+  --clr-green: hsl(125, 85%, 40%);
+  --clr-green-opacity: hsla(125, 85%, 40%, 0.555);
+  --clr-green-2: hsl(125, 88%, 35%);
+  --clr-red: hsl(0, 74%, 52%);
+  --clr-red-2: hsl(0, 76%, 45%);
+
+
+  --portrait-size: 60px;
+}
+/* #endregion */
+
+body{
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  background-image: url("https://s2.best-wallpaper.net/wallpaper/iphone/1903/Vineyard-village-sunshine-morning_iphone_1080x1920.jpg");
+  background-repeat: no-repeat;
+}
+
+.logo{
+  width: 250px;
+  height: 250px;
+  position: absolute;
+  padding: 3px;
+  background-color: green;
+  border-radius: 3px;
+  top: 5px;
+  right: 5px;
+  backdrop-filter: blur(2px);
+}
+
+.hidden{
+  visibility: hidden;
+}
+
+.container {
+    width: auto;
+    height: 800px;
+}
+
+.Tuyaux {
+    width: 50%;
+    justify-content: right;
+    position: relative;
+    background-color: #fff;
+    border: 5 solid #0d172a;
+    border-radius: 1.5rem;
+    box-sizing: border-box;
+    color: #0d172a;
+    cursor: pointer;
+    display: inline-block;
+    font-family: "Segoe UI", sans-serif;
+    font-size: 2rem;
+    font-weight: 600;
+    line-height: 1;
+    padding: 1rem 1.6rem;
+    text-align: center;
+    text-decoration: none #0d172a solid;
+    text-decoration-thickness: auto;
+    transition: all .1s cubic-bezier(.4, 0, .2, 1);
+    box-shadow: 0px 1px 2px rgba(166, 175, 195, 0.25);
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    margin-top: auto;
+    margin-left: 25%;
+    top: 50%;
+}
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+</style>
+
+
+)=====";
---
+++ Projects/Serveur web engiv/src/main.cpp
@@ -0,0 +1,179 @@
+#include <Arduino.h>
+
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+// Load Wi-Fi library
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include "index.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port D2 on the Arduino
+#define ONE_WIRE_BUS 4
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+// Replace with your network credentials
+const char* ssid     = "Le J c'est le S";
+const char* password = "eyvq9619381";
+
+// Set web server port number to 80
+ESP8266WebServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String output5State = "off";
+String output4State = "off";
+
+// Assign output variables to GPIO pins
+const int output5 = 5;
+const int output4 = 4;
+
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+void handleRoot() {
+ String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
+}
+
+void handleCapteur() {
+ float a = sensors.getTempCByIndex(0);
+ String b = String(a);
+  
+server.send(200, "text/plane", b);
+ 
+}
+
+void setup() {
+  Serial.begin(115200);
+  // Initialize the output variables as outputs
+  pinMode(output5, OUTPUT);
+  pinMode(output4, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output5, LOW);
+  digitalWrite(output4, LOW);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.on("/", handleRoot);
+  server.on("/readCapteur", handleCapteur);
+  server.begin();
+
+  sensors.begin();
+}
+
+void loop(){
+  server.handleClient();
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println(sensors.getTempCByIndex(0));
+
+  // WiFiClient client = server.available();   // Listen for incoming clients
+
+  // if (client) {                             // If a new client connects,
+  //   Serial.println("New Client.");          // print a message out in the serial port
+  //   String currentLine = "";                // make a String to hold incoming data from the client
+  //   currentTime = millis();
+  //   previousTime = currentTime;
+  //   while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+  //     currentTime = millis();         
+  //     if (client.available()) {             // if there's bytes to read from the client,
+  //       char c = client.read();             // read a byte, then
+  //       Serial.write(c);                    // print it out the serial monitor
+  //       header += c;
+  //       if (c == '\n') {                    // if the byte is a newline character
+  //         // if the current line is blank, you got two newline characters in a row.
+  //         // that's the end of the client HTTP request, so send a response:
+  //         if (currentLine.length() == 0) {
+  //           // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+  //           // and a content-type so the client knows what's coming, then a blank line:
+  //           client.println("HTTP/1.1 200 OK");
+  //           client.println("Content-type:text/html");
+  //           client.println("Connection: close");
+  //           client.println();
+            
+  //           // turns the GPIOs on and off
+  //           if (header.indexOf("GET /5/on") >= 0) {
+  //             Serial.println("GPIO 5 on");
+  //             output5State = "on";
+  //             digitalWrite(output5, HIGH);
+  //           } else if (header.indexOf("GET /5/off") >= 0) {
+  //             Serial.println("GPIO 5 off");
+  //             output5State = "off";
+  //             digitalWrite(output5, LOW);
+  //           } else if (header.indexOf("GET /4/on") >= 0) {
+  //             Serial.println("GPIO 4 on");
+  //             output4State = "on";
+  //             digitalWrite(output4, HIGH);
+  //           } else if (header.indexOf("GET /4/off") >= 0) {
+  //             Serial.println("GPIO 4 off");
+  //             output4State = "off";
+  //             digitalWrite(output4, LOW);
+  //           }
+            
+  //           // Display the HTML web page
+  //           client.println("<!DOCTYPE html><html>");
+  //           client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+  //           client.println("<link rel=\"icon\" href=\"data:,\">");
+  //           // CSS to style the on/off buttons 
+  //           // Feel free to change the background-color and font-size attributes to fit your preferences
+  //           client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+  //           client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+  //           client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+  //           client.println(".button2 {background-color: #77878A;}</style></head>");
+            
+  //           // Web Page Heading
+  //           client.println("<body><h1>ESP8266 Web Server TEST</h1>");
+            
+  //           // Display current state, and ON/OFF buttons for GPIO 5  
+  //           client.println("<p>GPIO 5 - State " + output5State + "</p>");
+  //           // If the output5State is off, it displays the ON button       
+  //           if (output5State=="off") {
+  //             client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+  //           } else {
+  //             client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+  //           } 
+               
+  //           // Display current state, and ON/OFF buttons for GPIO 4  
+  //           client.println("<p>GPIO 4 - State " + output4State + "</p>");
+  //           // If the output4State is off, it displays the ON button       
+  //           if (output4State=="off") {
+  //             client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+  //           } else {
+  //             client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+  //           }
+  //           client.println("</body></html>");
+            
+  //           // The HTTP response ends with another blank line
+  //           client.println();
+  //           // Break out of the while loop
+  //           break;
+  //         } else { // if you got a newline, then clear currentLine
+  //           currentLine = "";
+  //         }
+  //       } else if (c != '\r') {  // if you got anything else but a carriage return character,
+  //         currentLine += c;      // add it to the end of the currentLine
+  //       }
+}
